d0fb78d8c11974188a77874b406b7bf5f5074dd5
Vince Mansel <code@vincemansel.com>
2023-01-15 14:59:17 -0800

    Ensure configureView function is marked as final to prevent overrides
    Fix up formatting

--- IKSettings1/Controllers/BaseSettingsVC.swift
+++ IKSettings1/Controllers/BaseSettingsVC.swift
@@ -31,7 +31,7 @@ class BaseSettingsVC: UIViewController, SettingsCapable {
     configureView()
   }
   
-  func configureView() {
+  final func configureView() {
     title = settingsTitle
     navigationController?.navigationBar.prefersLargeTitles = true
     
@@ -64,41 +64,41 @@ class BaseSettingsVC: UIViewController, SettingsCapable {
 }
 
 extension BaseSettingsVC: UITableViewDataSource, UITableViewDelegate {
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-  return settingsDataSource.configuration.count
-}
-
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-  let datasource = settingsDataSource.configuration[indexPath.row]
-  let title = datasource.title
-  switch datasource.settingType {
-    case .info:
-      return configureSettingsCell(title: title, infoText: datasource.info ?? "", at: indexPath)
-    case .action:
-      return configureActionCell(title: title, handler: datasource.actionHandler!, at: indexPath)
-    case .group:
-      return configureActionCell(title: title, handler: datasource.actionHandler!, at: indexPath)
-    case .toggle:
-      return configureToggleCell(title: title, defaultsKey: datasource.info ?? "", at: indexPath)
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return settingsDataSource.configuration.count
   }
-}
-
-private func configureSettingsCell(title: String, infoText: String, at indexPath: IndexPath) -> IKSettingsCell {
-  guard let cell = tableView.dequeueReusableCell(withIdentifier: IKSettingsCell.reuseId, for: indexPath) as? IKSettingsCell else { return UITableViewCell() as! IKSettingsCell }
-  cell.set(title: title, infoText: infoText)
-  return cell
-}
-
-private func configureActionCell(title: String, handler: @escaping TapHandler, at indexPath: IndexPath) -> IKActionCell {
-  guard let cell = tableView.dequeueReusableCell(withIdentifier: IKActionCell.reuseId, for: indexPath) as? IKActionCell else { return UITableViewCell() as! IKActionCell }
-  cell.set(title: title, content: IKActionCell.Content(tapHandler: handler))
-  return cell
-}
-
-private func configureToggleCell(title: String, defaultsKey: String, at indexPath: IndexPath) -> IKToggleCell {
-  guard let cell = tableView.dequeueReusableCell(withIdentifier: IKToggleCell.reuseId, for: indexPath) as? IKToggleCell else { return UITableViewCell() as! IKToggleCell}
-  cell.set(title: title, defaultsKey: defaultsKey)
-  return cell
-}
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let datasource = settingsDataSource.configuration[indexPath.row]
+    let title = datasource.title
+    switch datasource.settingType {
+      case .info:
+        return configureSettingsCell(title: title, infoText: datasource.info ?? "", at: indexPath)
+      case .action:
+        return configureActionCell(title: title, handler: datasource.actionHandler!, at: indexPath)
+      case .group:
+        return configureActionCell(title: title, handler: datasource.actionHandler!, at: indexPath)
+      case .toggle:
+        return configureToggleCell(title: title, defaultsKey: datasource.info ?? "", at: indexPath)
+    }
+  }
+  
+  private func configureSettingsCell(title: String, infoText: String, at indexPath: IndexPath) -> IKSettingsCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: IKSettingsCell.reuseId, for: indexPath) as? IKSettingsCell else { return UITableViewCell() as! IKSettingsCell }
+    cell.set(title: title, infoText: infoText)
+    return cell
+  }
+  
+  private func configureActionCell(title: String, handler: @escaping TapHandler, at indexPath: IndexPath) -> IKActionCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: IKActionCell.reuseId, for: indexPath) as? IKActionCell else { return UITableViewCell() as! IKActionCell }
+    cell.set(title: title, content: IKActionCell.Content(tapHandler: handler))
+    return cell
+  }
+  
+  private func configureToggleCell(title: String, defaultsKey: String, at indexPath: IndexPath) -> IKToggleCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: IKToggleCell.reuseId, for: indexPath) as? IKToggleCell else { return UITableViewCell() as! IKToggleCell}
+    cell.set(title: title, defaultsKey: defaultsKey)
+    return cell
+  }
 }
 
